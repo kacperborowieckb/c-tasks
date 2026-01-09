e7c62fdce8315170846532e314d596cacbe51b0e
kacperborowieckb <kacperborowiec.kb@gmail.com>
2026-01-09 18:29:44 +0100

    feat: l4 p1

--- LEKCJA4/p1.c
+++ LEKCJA4/p1.c
@@ -12,7 +12,6 @@ sem_wait(sem_t* s)	- operacja wait
 
 **************************************************************/
 
-
 /*************  korzystanie z mutexu  ******************************
 
 
@@ -22,13 +21,12 @@ pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER - inicjowanie (mutex otwarty) (d
 pthread_mutex_lock(pthread_mutex* m) 	- zamknięcie mutexu
 pthread_mutex_unlock(pthread_mutex* m) 	- otwarcie mutexu
 
-uwaga: 
+uwaga:
 - operacja pthread_mutex_lock blokuje wątek gdy mutex jest zamkniety
 - operacja pthread_mutex_unlock jest niezdefiniowana gdy mutex jest otwarty
 
 
 **************************************************************/
-
 
 #include <stdio.h>
 #include <unistd.h>
@@ -36,69 +34,80 @@ uwaga:
 #include <pthread.h>
 #include <semaphore.h>
 
-#define LP   1
-#define LQ   1
-#define SIZE  2
+#define LP 1
+#define LQ 1
+#define SIZE 2
 #define ILOSC 100
+
 int bufor[SIZE];
 
+int creator_index = 0;
+int consumer_index = 0;
 
+sem_t sem_creator;
+sem_t sem_consumer;
+pthread_mutex_t mu = PTHREAD_MUTEX_INITIALIZER;
 
-void* p (void* l) { // funkcja watku (watek)
-      int i,x;
+void *p(void *l)
+{ // funkcja watku (watek)
+      int i, x;
 
-      for (i=0;i<ILOSC;i++) {
+      for (i = 0; i < ILOSC; i++)
+      {
+            x = rand() % 100;
 
-      x=i+1;
-     
+            sem_wait(&sem_creator);
+            pthread_mutex_lock(&mu);
+
+            bufor[creator_index] = x;
+            creator_index = (creator_index + 1) % SIZE;
+
+            pthread_mutex_unlock(&mu);
+            sem_post(&sem_consumer);
       }
 
-      
-
-return 0;     
+      return 0;
 }
 
+void *q(void *l)
+{ // funkcja watku (watek)
 
-void* q (void* l) { // funkcja watku (watek)
-     
-      int i,x;
+      int i, x;
 
-      for (i=0;i<ILOSC;i++) {
-    
+      for (i = 0; i < ILOSC; i++)
+      {
+            sem_wait(&sem_consumer);
+            pthread_mutex_lock(&mu);
 
-      printf("x=%d\n",x);
+            x = bufor[consumer_index];
+            consumer_index = (consumer_index + 1) % SIZE;
+            printf("x=%d\n", x);
+
+            pthread_mutex_unlock(&mu);
+            sem_post(&sem_creator);
       }
 
-
-return 0;     
+      return 0;
 }
 
+int main()
+{
+      pthread_t wp[LP], wq[LQ];
 
+      sem_init(&sem_creator, 0, SIZE);
+      sem_init(&sem_consumer, 0, 0);
 
-int main () {
-	pthread_t wp[LP],wq[LQ];
-	
-   int i;
+      int i;
 
-  for (i=0;i<LP;i++)		
-  pthread_create(&wp[i], 0, p,0); // tworzy nowy watek 		
-  for (i=0;i<LQ;i++)
-  pthread_create(&wq[i], 0, q,0); // tworzy nowy watek 				
-	
+      for (i = 0; i < LP; i++)
+            pthread_create(&wp[i], 0, p, 0); // tworzy nowy watek
+      for (i = 0; i < LQ; i++)
+            pthread_create(&wq[i], 0, q, 0); // tworzy nowy watek
 
+      for (i = 0; i < LP; i++)
+            pthread_join(wp[i], 0); // czeka na zakonczenie watku 1
+      for (i = 0; i < LQ; i++)
+            pthread_join(wq[i], 0);
 
-
-
-
-
-
-  for (i=0;i<LP;i++)
-  pthread_join(wp[i],0); 	// czeka na zakonczenie watku 1
-  for (i=0;i<LQ;i++)
-  pthread_join(wq[i],0);     
-  
-
-  
-  
-return 0;
+      return 0;
 }
